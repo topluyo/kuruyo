c64fca22ae6729e41f0cb40006b8552a6391491d
Hasan Delibaş <hasan.delibas94@gmail.com>
2026-03-07 00:06:17 +0300

    Remove sudo from Go installation script

--- server/install/go.sh
+++ server/install/go.sh
@@ -2,20 +2,20 @@ echo "==========================================="
 echo "==              GOLANG                  ==="
 echo "==========================================="
 
-sudo apt update
-sudo apt install -y git
+apt update
+apt install -y git
 
-sudo rm -rf /usr/local/go
-sudo rm -rf /usr/bin/go
-sudo apt remove -y golang-go 
+rm -rf /usr/local/go
+rm -rf /usr/bin/go
+apt remove -y golang-go 
 
 
 
 GO_VERSION="1.22.6"
 
 wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 # Add to ~/.profile ONLY if not already added
 PROFILE="$HOME/.profile"
 
@@ -26,6 +26,7 @@ grep -qxF 'export GOPATH=$HOME/go' "$PROFILE" || \
 grep -qxF 'export PATH=$PATH:$GOPATH/bin' "$PROFILE" || \
     echo 'export PATH=$PATH:$GOPATH/bin' >> "$PROFILE"
 source "$PROFILE"
+source ~/.profile
 go version
 
 rm go${GO_VERSION}*.tar.gz
