04042d567d1d5821d7dcc9818e03d26341b5024f
Hasan Delibaş <hasan.delibas94@gmail.com>
2026-03-06 23:57:46 +0300

    Create php.sh for PHP installation
    Add a script to install PHP and its extensions.

---
+++ server/install/php.sh
@@ -0,0 +1,6 @@
+echo "==========================================="
+echo "==              PHP                     ==="
+echo "==========================================="
+
+sudo apt update
+sudo apt install -y php php-cli php-fpm php-curl php-mbstring php-xml php-zip php-mysql php-gd
