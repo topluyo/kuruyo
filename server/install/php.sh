3f518e1e49cc2e48d04335b0084597f51fdc8ee9
Hasan Delibaş <hasan.delibas94@gmail.com>
2026-03-07 00:19:54 +0300

    Upgrade PHP installation to version 8.2

--- server/install/php.sh
+++ server/install/php.sh
@@ -3,4 +3,4 @@ echo "==              PHP                     ==="
 echo "==========================================="
 
 apt update
-apt install -y php php-cli php-fpm php-curl php-mbstring php-xml php-zip php-mysql php-gd
+apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-curl php8.2-mbstring php8.2-xml php8.2-zip php8.2-mysql php8.2-gd
