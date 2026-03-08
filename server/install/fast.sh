5eaa934bf9fb4c484d99be96bc0169215033c293
Hasan Delibaş <hasan.delibas94@gmail.com>
2026-03-08 23:25:58 +0300

    Fix sysctl command by removing sudo

--- server/install/fast.sh
+++ server/install/fast.sh
@@ -3,4 +3,4 @@ echo "==              FAST NETWORK            ==="
 echo "==========================================="
 
 mv /web/server/install/sysctl.conf /etc/sysctl.conf
-sudo sysctl -p
+sysctl -p
