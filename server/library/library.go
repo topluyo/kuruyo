a79c74ebb1612e2ace119572190939196fad2b33
Hasan Delibaş <hasan.delibas94@gmail.com>
2026-03-14 05:48:39 +0300

    Create library.go

---
+++ server/library/library.go
@@ -0,0 +1,73 @@
+package main
+
+import(
+	"os"
+	"strings"
+	"unicode"
+	"strconv"
+	"log"
+)
+
+func argument(key string, defaults ...string) string {
+	response := ""
+	for _, arg := range os.Args {
+		if strings.HasPrefix(arg, key+"=") {
+			response = strings.SplitN(arg, "=", 2)[1]
+			response = strings.Trim(response, "\"")
+			return response
+		}
+	}
+	if len(defaults) > 0 {
+		return defaults[0]
+	}
+	return ""
+}
+
+func enviroment(a string) string{
+	return os.Getenv(a)
+}
+
+func write(values ...interface{}) {
+	originalFlags := log.Flags()
+	log.SetFlags(0)
+	log.Println(values...)
+	log.SetFlags(originalFlags)
+}
+
+func center(text string) string {
+	const width = 48
+	if len(text) >= width {
+		return text // return as-is if longer than width
+	}
+	padding := (width - len(text)) / 2
+	return strings.Repeat(" ", padding) + text + strings.Repeat(" ", width-len(text)-padding)
+}
+
+func table(name string){
+	// █
+	write("╔════════════════════════════════════════════════╗") // 50 Char
+	write("║"+center(name)+"║")
+	write("╚════════════════════════════════════════════════╝") // 50 Char
+}
+
+func row(name string){
+	// █
+	write("┌────────────────────────────────────────────────┐") // 50 Char
+	write("│"+center(name)+"│")
+	write("└────────────────────────────────────────────────┘") // 50 Char
+}
+
+func ToNumber(number string) int {
+	numStr := ""
+	for _, r := range number {
+		if unicode.IsDigit(r) {
+			numStr += string(r)
+		} else {
+			break
+		}
+	}
+	if numStr == "" { return 0 }
+	num, err := strconv.Atoi(numStr)
+	if err != nil { return 0 }
+	return num
+}
