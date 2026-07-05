96496585affacbc718620eeebb9d141d08ad0e35
soforces <r2ataolkaratoprak@gmail.com>
2026-07-05 03:43:10 +0300

    docs: fix broken install command and mojibake in README
    GitHub's archive/refs/heads/master.zip extracts to `kuruyo-master/`,
    not `kuruyo-main/`, so the documented `mv` step failed for anyone
    following the quick-install instructions verbatim. Also removes two
    stray U+FFFD replacement characters left over from an encoding issue
    in two README headings.

--- docs/install.sh
+++ docs/install.sh
@@ -3,5 +3,5 @@ apt upgrade
 apt install curl unzip
 curl -L -o kuruyo.zip https://github.com/topluyo/kuruyo/archive/refs/heads/master.zip
 unzip kuruyo.zip
-mv kuruyo-main/ /web
+mv kuruyo-master/ /web
 rm kuruyo.zip
